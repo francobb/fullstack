bfc8369eed1b05b26e5b260239e01a6e629df1c9
bfrancois <bfrancois@kenzan.com>
2018-04-04 23:32:04 -0400

    add prod env

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,5 @@
-module.exports = {
-	clientId: '505886538057-5a69h4psd4m0api5jnu549p9bh8toh8d.apps.googleusercontent.com',
-	clientSecret: 'BPMOuYUDcgYwJ6ADNJD85oGP',
-	mongoURI: 'mongodb://bfrancois:baller12@ds127899.mlab.com:27899/emaily',
-	cookieKey: 'oubnifbfiubfeifbeeoj',
-};
+if (node.process.ENV === 'production') {
+	module.exports = require('./prod');
+} else {
+	module.exports = require('./dev');
+}
